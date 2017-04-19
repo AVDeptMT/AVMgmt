c7f3b6d1981c3754ca1699adcae5cf1f93c0c708
mmcfall2 <mpm3t@mtmail.mtsu.edu>
2017-04-18 21:24:49 -0500

    updated seproject folder and files
    lots of changes

--- seproject/testconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con=mysqli_connect("cemserver.fsa.mtsu.edu:80","root","Mtsu2017!","test");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
